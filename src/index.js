7fc3ac3d9c636f6288d79fce2647d643935065c5
jongmin <charlieko123@github.com>
2024-10-10 11:03:11 +0900

    init: 폴더 추가

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<div>123</div>);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
