822a17877c58889d29d453dfaad4fc756fa72dc0
jongmin <charlieko123@github.com>
2024-10-10 14:35:18 +0900

    style: 모바일 사이즈 반응형 추가

--- src/components/DropdownNormal.jsx
+++ src/components/DropdownNormal.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import styles from "../styles/DropdownNormal.module.css";
 
 import downArrow from "../images/icon_arrowdown.svg";
+import filterIcon from "../images/icon_filter.svg";
 
 export function DropdownNormal({ title = "Select", options = [] }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,7 +19,13 @@ export function DropdownNormal({ title = "Select", options = [] }) {
   return (
     <div className={styles.dropdownNormal}>
       <button className={styles.normalButton} onClick={toggleDropdown}>
-        {selectedOption}
+        {/* 텍스트 (데스크탑, 태블릿) */}
+        <span className={styles.buttonText}>{selectedOption}</span>
+        {/* 아이콘 (모바일) */}
+        <div className={styles.buttonIcon}>
+          <img src={filterIcon} alt="Dropdown Icon" />
+        </div>
+
         <img
           src={downArrow}
           alt="Dropdown Icon"
