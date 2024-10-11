bd4837ac3df436f74ea278c96dc7dc248d4fe2db
jongmin <charlieko123@github.com>
2024-10-11 14:32:10 +0900

    style: original imgCard css 작성 및 테스트

--- src/pages/test_page/Test.jsx
+++ src/pages/test_page/Test.jsx
@@ -5,6 +5,9 @@ import {
 } from "../../components/commons/dropdown_normal/DropdownNormal";
 import { DropdownInput } from "../../components/commons/dropdown_input/DropdownInput";
 import "./Test.css";
+import ImgCardOriginal from "../../components/imgcard_original/ImgCardOriginal";
+
+import defaultImg from "./assets/image1.svg";
 
 const DEFAULT_SELECT_OPTION = "등급";
 const DEFAULT_SELECT_OPTIONS = ["COMMON", "RARE", "SUPER RARE", "LEGENDARY"];
@@ -15,26 +18,32 @@ const DEFAULT_ORDER_OPTIONS = [
   "높은 가격순",
   "낮은 가격순",
 ];
+const testCardData = {
+  title: "우리집 앞마당",
+  grade: "COMMON",
+  genre: "평점",
+  nickname: "미쓰쏘",
+  price: 4,
+  counts: 2,
+  maxCounts: 5,
+  imageUrl: defaultImg,
+};
 
 function Test() {
   return (
     <div className="test">
       <div className="testContainer1">
-        <DropdownNoneBorder
-          title={DEFAULT_SELECT_OPTION}
-          options={DEFAULT_SELECT_OPTIONS}
-        />
-        <DropdownBorder
-          title={DEFAULT_ORDER_OPTION}
-          options={DEFAULT_ORDER_OPTIONS}
-        />
+        <ImgCardOriginal {...testCardData} />
+        <ImgCardOriginal {...testCardData} />
+        <ImgCardOriginal {...testCardData} />
+        <ImgCardOriginal {...testCardData} />
       </div>
       <div className="testContainer2">
-        <DropdownInput
+        {/* <DropdownInput
           label={DEFAULT_SELECT_OPTION}
           placeholder={`${DEFAULT_SELECT_OPTION}을 선택해주세요.`}
           options={DEFAULT_SELECT_OPTIONS}
-        />
+        /> */}
       </div>
     </div>
   );
