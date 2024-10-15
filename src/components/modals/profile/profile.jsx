562dab4460310f5d60e304988d6ae153c1f14626
Jun5 <chdh1005@gmail.com>
2024-10-15 19:01:45 +0900

    Chore: Merge되지 않은 파일들을 임의로 다운로드 및 로컬로 이동

---
+++ src/components/modals/profile/profile.jsx
@@ -0,0 +1,30 @@
+import style from "./profile.module.css";
+
+const Profile = () => {
+  return (
+    <div className={style.profile_Wrapper}>
+      <div className={style.profile_Container}>
+        <div className={style.profile_info_Container}>
+          <div className={style.profile_topArea}>
+            <p className={style.top_helloUser}>
+              안녕하세요, <span>유디</span>님!
+            </p>
+            <div className={style.top_point_Wrapper}>
+              <span className={style.top_point_text}>보유 포인트</span>
+              <span className={`${style.top_point_text} ${style.point}`}>
+                1,540p
+              </span>
+            </div>
+          </div>
+          <div className={style.profile_bottomArea}>
+            <span className={style.bottom_text}>마이겔러리</span>
+            <span className={style.bottom_text}>나의 판매 포토카드</span>
+          </div>
+        </div>
+        <span className={style.logout}>로그아웃</span>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
