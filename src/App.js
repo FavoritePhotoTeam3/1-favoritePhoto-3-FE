12ac81e8de538e75c8fa7402a24a4598a8f0ee93
TradeOffEgoist <egoist116@naver.com>
2024-10-16 15:16:49 +0900

    complete nav state handler

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 import "./styles/reset.css";
 import LoginPage from "./page/login";
 import JoinPage from "./page/join";
+import CompleteTradePage from "./page/completeTrade";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<span>언젠가 만나 네게 할 말이 많아.</span>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/join" element={<JoinPage />} />
+        <Route path="/completeTrade" element={<CompleteTradePage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/page/completeTrade/index.jsx
@@ -0,0 +1,11 @@
+import { Nav } from "../../components/nav/nav";
+import style from "./index.module.css";
+const CompleteTradePage = () => {
+  return (
+    <div className={style.trade_Wrapper}>
+      <Nav />
+    </div>
+  );
+};
+
+export default CompleteTradePage;
