723d38fd543d6094a4b26909e31179bdc9ab1c7a
TradeOffEgoist <egoist116@naver.com>
2024-10-17 09:00:16 +0900

    nav position: absolute

--- src/page/completeTrade/index.jsx
+++ src/page/completeTrade/index.jsx
@@ -1,9 +1,17 @@
 import { Nav } from "../../components/nav/nav";
 import style from "./index.module.css";
+import logo from "./assets/logo.png";
 const CompleteTradePage = () => {
   return (
     <div className={style.trade_Wrapper}>
       <Nav />
+      <div className={style.trade_Container}>
+        <img className={style.trade_logo} src={logo} alt="" />
+        <span className={style.trade_text}>포토카드 교환에 성공했습니다!</span>
+        <button className={style.tarde_reDirection}>
+          나의 판매 포토카드에서 확인하기
+        </button>
+      </div>
     </div>
   );
 };
