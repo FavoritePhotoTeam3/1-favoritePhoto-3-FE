f3fc4a0c87138d51f6f7fe00986116aa16c4f3d4
Jun5 <chdh1005@gmail.com>
2024-10-22 14:56:10 +0900

    Feat: 간단한 Redux 로깅 미들웨어 추가 > console.log로 작동

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { errorLoggerMiddleware } from "./errorLoggerMiddleware";
 
 import { loginSlice } from "../feature/login/loginSlice";
 import { signupSlice } from "../feature/signup/signupSlice";
 import { authSlice } from "../route/authSlice";
+
+import { shopSlice } from "../feature/shop_card/shopCardSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     login: loginSlice.reducer,
     signup: signupSlice.reducer,
+    shop: shopSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(errorLoggerMiddleware), // errorLoggerMiddleware 추가
 });
 
 export default store;
