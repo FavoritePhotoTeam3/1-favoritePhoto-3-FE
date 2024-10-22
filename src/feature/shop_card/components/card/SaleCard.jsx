fe32c93ea467d29131013ac9afd9814e3d421b73
Jun5 <chdh1005@gmail.com>
2024-10-22 15:17:04 +0900

    Refactor: 기존 img original Card 컴포넌트 재사용 및 리팩토링

---
+++ src/feature/shop_card/components/card/SaleCard.jsx
@@ -0,0 +1,82 @@
+import React from "react";
+import style from "./SaleCard.module.css";
+import { Link } from "react-router-dom";
+
+import logoImage from "./assets/logo.svg";
+import soldOutImage from "./assets/soldout.svg";
+
+const ImgCardOriginal = ({ data }) => {
+  
+  const price = data.price;
+  const totalCount = data.totalCount;
+  const remainingCount = data.remainingCount;
+  const isSoldOut = data.isSoldOut;
+
+  const name = data.card.name;
+  const genre = data.card.genre;
+  const grade = data.card.grade;
+  const imageURL = data.card.imageURL;
+
+  const nickname = data.user.nickname;
+
+  const gradeStyle = grade
+    ? grade.replace(/\s+/g, "").toLowerCase()
+    : "";
+  console.log(`데이터 등급 : ${grade}`);
+
+
+
+  return (
+    <Link to={`/item/${data?.id}`}>
+      <figure className={style.contaier}>
+        <section className={style.imgWrapper}>
+          <img
+            src={imageURL}
+            alt="Card"
+            className={`${style.cardImage} ${isSoldOut ? style.soldOut : ""}`}
+          />
+          {isSoldOut && (
+            <img
+              src={soldOutImage}
+              alt="Sold out"
+              className={style.soldOutImage}
+            />
+          )}
+        </section>
+
+        <article className={style.contentSection}>
+          <section className={style.cardInfo}>
+            <p>{name}</p>
+            <div className={style.subtitle}>
+              <div className={style.infoLeftBox}>
+                <p className={style[gradeStyle]}>{grade.toUpperCase()}</p>
+                <p>|</p>
+                <p>{genre}</p>
+              </div>
+              <p className={style.nickname}>{nickname}</p>
+            </div>
+          </section>
+
+          <section className={style.quntitySection}>
+            <div className={style.price}>
+              <label>가격</label>
+              <p className={style.whiteCount}>{price}</p>
+            </div>
+            <div className={style.remain}>
+              <label>잔여</label>
+              <p className={style.whiteCount}>
+                {remainingCount}
+                <span
+                  className={style.grayCount}
+                >{` / ${totalCount}`}</span>
+              </p>
+            </div>
+          </section>
+        </article>
+        <img src={logoImage} alt="logo icon" className={style.logoImage} />
+      </figure>
+    </Link>
+  );
+};
+
+export default React.memo(ImgCardOriginal);
