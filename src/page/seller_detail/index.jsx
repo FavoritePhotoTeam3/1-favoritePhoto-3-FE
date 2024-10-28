349678bc226ecb5f1c6cbc587cf64d894d49003d
jongmin <charlieko123@github.com>
2024-10-28 13:13:39 +0900

    chore: 상세페이지 로고, back 아이콘 클릭시 홈 리다이렉트 추가

--- src/page/seller_detail/index.jsx
+++ src/page/seller_detail/index.jsx
@@ -2,7 +2,7 @@ import styles from "./index.module.css";
 import { TitleDetail } from "../../components/common/title/Title";
 import DescCardSeller from "@components/desc_card/desc_card_seller/DescCardSeller";
 import ImgCardExchange from "../../components/imgcard_exchange/ImgCardExchange";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import {
   useSellerDetail,
@@ -24,6 +24,7 @@ import backIcon from "./assets/back_icon.svg";
 
 const SellerDetailPage = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const { shopId } = useParams();
 
   const { isModalOpen, modalType } = useSelector((state) => state.sellerModal);
@@ -42,6 +43,8 @@ const SellerDetailPage = () => {
     isLoading: exchangeLoading,
     refetch: refetchExchangeCards,
   } = useSellerExchangeCards(shopId);
+
+  const handleHomeClick = () => navigate("/");
 
   const handleRefresh = () => {
     console.log("새로고침 버튼 클릭됨");
@@ -131,8 +134,13 @@ const SellerDetailPage = () => {
       <main className={styles.main}>
         <div className={styles.mainContainer}>
           <div className={styles.header}>
-            <img src={backIcon} alt="back" className={styles.backIcon} />
-            <div className={styles.logoWrapper}>
+            <img
+              src={backIcon}
+              alt="back"
+              className={styles.backIcon}
+              onClick={handleHomeClick}
+            />
+            <div className={styles.logoWrapper} onClick={handleHomeClick}>
               <p className={styles.logo}>마켓플레이스</p>
             </div>
           </div>
